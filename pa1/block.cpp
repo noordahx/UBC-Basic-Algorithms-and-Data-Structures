cdc79db435b7f240c0002ed1ddc9ec574aee873a
Nurda <noordah0@students.ubc.ca>
2022-10-17 18:50:17 -0700

    block

--- pa1/block.cpp
+++ pa1/block.cpp
@@ -2,36 +2,72 @@
 #include <cmath>
 #include <iostream>
 
+
 int Block::width() const
 {
 /* YOUR CODE HERE */
+    return data[0].size();
 }
 int Block::height() const
 {
 /* YOUR CODE HERE */
+    return data.size();
 }
 
 void Block::render(PNG &im, int column, int row) const
 {
 /* YOUR CODE HERE */
+    for (int x = column; x < column+width(); x++) {
+        for (int y = row; y < row+height(); y++) {
+            *im.getPixel(x,y) = data[x][y];
+        }
+    }
+
 }
 
 void Block::build(PNG &im, int column, int row, int width, int height)
 {
 /* YOUR CODE HERE */
+    for (int x = column; x < column+width; x++) {
+        for (int y = row; y < row+height; y++) {
+            data[x][y] = *im.getPixel(x,y);
+        }
+    }
+    
 }
 
 void Block::flipVert()
 {
 /* YOUR CODE HERE */
+    for (int x = 0; x < width(); x++) {
+        for (int y = 0; y < height()/2; y++) {
+            RGBAPixel temp = data[x][y];
+            data[x][y] = data[x][height()-1-y];
+            data[x][height()-1-y] = temp;
+        }
+    }
 }
 
 void Block::flipHoriz()
 {
 /* YOUR CODE HERE */
+    for (int x = 0; x < width()/2; x++) {
+        for (int y = 0; y < height(); y++) {
+            RGBAPixel temp = data[x][y];
+            data[x][y] = data[width()-1-x][y];
+            data[width()-1-x][y] = temp;
+        }
+    }
 }
 
 void Block::rotateRight()
 {
 /* YOUR CODE HERE */
+    for (int x = 0; x < width(); x++) {
+        for (int y = 0; y < height(); y++) {
+            RGBAPixel temp = data[x][y];
+            data[x][y] = data[width()-1-x][height()-1-y];
+            data[width()-1-x][height()-1-y] = temp;
+        }
+    }
 }
