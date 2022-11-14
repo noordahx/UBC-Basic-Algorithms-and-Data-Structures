471f822bac9539dfba852f73e9037de50e0d40bf
Noordahx <taumergenov@protonmail.com>
2022-11-14 01:55:27 -0800

    add: neighbors function P2

--- pa2/treasureMap.cpp
+++ pa2/treasureMap.cpp
@@ -6,7 +6,10 @@ treasureMap::treasureMap(const PNG & baseim, const PNG & mazeim, pair<int,int> s
 {
 
 /* YOUR CODE HERE */
-
+    base = PNG(baseim);
+    maze = PNG(mazeim);
+    start.first = s.first;
+    start.second = s.second;
 }
 
 void treasureMap::setGrey(PNG & im, pair<int,int> loc){
@@ -43,6 +46,11 @@ bool treasureMap::good(vector<vector<bool>> & v, pair<int,int> curr, pair<int,in
 vector<pair<int,int>> treasureMap::neighbors(pair<int,int> curr) {
 
 /* YOUR CODE HERE */
-
+    vector<pair<int,int>> result;
+    result.push_back(make_pair(curr.first, curr.second - 1));
+    result.push_back(make_pair(curr.first - 1, curr.second));
+    result.push_back(make_pair(curr.first, curr.second + 1));
+    result.push_back(make_pair(curr.first + 1, curr.second));
+    return result;
 }
 
